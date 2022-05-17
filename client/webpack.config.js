76b53e3978ae07a0fcd13ffe95df6540a76ba5d3
Crucix <fchang1720@gmail.com>
2022-05-17 17:30:52 -0500

    finished adding plugins for generating manifests

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -21,12 +21,31 @@ module.exports = () => {
 
       new HtmlWebpackPlugin({
         template: './index.html',
-        title: 'Jate Text Editor'
+        title: 'JATE Text Editor'
       }),
 
       new InjectManifest({
         swSrc: './src-sw.js',
         swDest: 'src-sw.js',
+      }),
+
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'JATE Text Editor',
+        short_name: 'JATE',
+        description: 'Just Another Text Editor',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: '/',
+        publicPath: '/',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
       }),
       
     ],
