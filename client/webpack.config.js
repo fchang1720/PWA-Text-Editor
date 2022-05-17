b250f4e590a2a7388138ea87a43b556d47fbdcc8
Crucix <fchang1720@gmail.com>
2022-05-17 17:33:39 -0500

    added babel loader to webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -56,6 +56,18 @@ module.exports = () => {
           test: /\.css$/i,
           use: ['style-loader', 'css-loader'],
         },
+
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
         
       ],
     },
