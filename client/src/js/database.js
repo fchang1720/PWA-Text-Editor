266f8a5517cf0ad981b76d950ef5ecc2adf8cdd7
Crucix <fchang1720@gmail.com>
2022-05-18 18:50:46 -0500

    fixed database functions

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,11 +13,11 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (id, content) => { console.log('PUT to the database');
+export const putDb = async (content) => { console.log('PUT to the database');
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  const request = store.put({ id: id, content: content });
+  const request = store.put({value: content });
   const result = await request;
   console.log('data saved to the database', result);
 };
@@ -27,7 +27,7 @@ export const getDb = async () => {console.log('GET from the database');
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readonly');
   const store = tx.objectStore('jate');
-  const request = store.getAll();
+  const request = store.get(1);
   const result = await request;
   console.log('result.value', result);
   return result;
